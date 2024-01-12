8e528573ecb9f3382c17f3810cc17ee726f7c2ba
robertpohl <robban.p@gmail.com>
2024-01-12 14:16:53 +0100

    error, validation

--- src/routes/invoice.rs
+++ src/routes/invoice.rs
@@ -1,5 +1,6 @@
 use axum::{Json, extract::{Path, Query}, http::{HeaderMap, header::USER_AGENT, StatusCode}, response::{IntoResponse, Response}};
 use serde::{Serialize, Deserialize};
+use validator::{Validate, ValidationError};
 
 pub async fn create_invoice(Json(body): Json<Invoice>) -> Json<Invoice> {
     dbg!(&body);
@@ -16,8 +17,8 @@ pub async fn get_invoice(
     println!("{:?}", agent_str);
 
     let invoice = Invoice {id: id,
-        amount: 10,
-        currency: "sek".into(),
+        amount: 17,
+        currency: "sekd".to_string(),
         filter: match params.filter {
             Some(value) => value,
             None => "unknown".to_string(),
@@ -31,6 +32,13 @@ pub async fn get_invoice(
             None => 0,
         },
     };
+
+
+    match invoice.validate() {
+        Ok(_) => (),
+        Err(e) => return (StatusCode::UNPROCESSABLE_ENTITY, Json(e)).into_response(),
+    };
+
     if invoice.filter == "sek".to_owned() {
         return (
             StatusCode::CREATED,
@@ -43,10 +51,12 @@ pub async fn get_invoice(
 pub struct ErrorMessage {
     pub message: String,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Validate)]
 pub struct Invoice {
     /// Need to be smaller number
+    #[validate(range(min = 18, max = 20))]
     amount: u16,
+    #[validate(custom = "validate_allowed_currencies")]
     pub currency: String,
     id: u32,
     filter: String,
@@ -59,4 +69,13 @@ pub struct QueryParams {
     filter: Option<String>,
     start: Option<u32>,
     end: Option<u32>,
+}
+
+fn validate_allowed_currencies(currency: &str) -> Result<(), ValidationError> {
+    dbg!(currency);
+     if currency != "sek".to_string() &&  currency != "eur"{
+        return Err(ValidationError::new("only sek or eur accepted"));
+    }
+
+    Ok(())
 }
